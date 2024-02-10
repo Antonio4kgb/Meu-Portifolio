e99ba13190eb691245e55db9e02a3033565bfcbb
antonio <antonio4kgb@gmail.com>
2024-02-10 12:48:57 -0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+/* scroll: animação da cor do navbar*/
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+});
